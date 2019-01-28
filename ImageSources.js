affd993f4a2ab0aa8c0d40823c507019a76f98af
Alessandro Petrillo <aless.petrillo@gmail.com>
2019-01-28 12:53:36 +0100

    Add needed pictures

--- ImageSources.js
+++ ImageSources.js
@@ -2,9 +2,48 @@ const ImageSources = {
     luckyPig: {
         uri: require('./assets/lucky_pig_icon.png')
     },
-  buzzer: {
-    uri: require('./assets/drawable/buzzer.png')
-  }
+    rulesPig: {
+        uri: require('./assets/drawable/rules_pig.png')
+    },
+    leueSpel: {
+        uri: require('./assets/drawable/leuespel.png')
+    },
+    deletePoints: {
+        uri: require('./assets/drawable/delete_points.png')
+    },
+    savePoints: {
+        uri: require('./assets/drawable/save_points.png')
+    },
+    buzzer: {
+        uri: require('./assets/drawable/buzzer.png')
+    },
+    vaultingBox: {
+        uri: require('./assets/drawable/vaulting_box.png')
+    },
+    vaultingHorse: {
+        uri: require('./assets/drawable/vaulting_horse.png')
+    },
+    mat: {
+        uri: require('./assets/drawable/mat.png')
+    },
+    rings: {
+        uri: require('./assets/drawable/rings.png')
+    },
+    longBench: {
+        uri: require('./assets/drawable/long_bench.png')
+    },
+    playIcon: {
+        uri: require('./assets/drawable/play.svg')
+    },
+    pauseIcon: {
+        uri: require('./assets/drawable/pause.svg') 
+    },
+    undoLastIcon: {
+        uri: require('./assets/drawable/undo.svg') 
+    },
+    stopIcon: {
+        uri: require('./assets/drawable/stop.svg')
+    }
 };
 
 export default ImageSources;
