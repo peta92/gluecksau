2a89b451cce26e597e459c5d0a3a7584ffa396b1
Alessandro Petrillo <aless.petrillo@gmail.com>
2019-03-05 15:35:50 +0100

    Add AppLoading at the start of the app so images can be cached

--- App.js
+++ App.js
@@ -1,16 +1,18 @@
 import React from 'react'
+import { Asset, AppLoading } from 'expo'
 import {createStackNavigator, createAppContainer } from 'react-navigation'
 import HomeScreen from './ScreenHome'
 import RulesScreen from './ScreenRules'
 import HistoryScreen from './ScreenHistory'
 import PlayScreen from './ScreenPlay'
+import ImageSources from './ImageSources'
 import './strings'
 
 const MainNavigator = createStackNavigator({
-  Home: {screen: HomeScreen},
-  Play: {screen: PlayScreen},
-  Rules: {screen: RulesScreen},
-  History: { screen: HistoryScreen },
+  Home: HomeScreen,
+  Play: PlayScreen,
+  Rules: RulesScreen,
+  History: HistoryScreen,
 }, {
   headerMode: "none",
 
@@ -19,7 +21,43 @@ const MainNavigator = createStackNavigator({
 const AppContainer = createAppContainer(MainNavigator)
 
 export default class App extends React.Component {
+  state = {
+    isReady: false,
+  };
+
   render() {
+    if (!this.state.isReady) {
+      return (
+        <AppLoading
+          startAsync={this._cacheResourcesAsync}
+          onFinish={() => this.setState({ isReady: true })}
+          onError={console.warn}
+        />
+      )
+    }
+
     return <AppContainer />
+
+  }
+
+  async _cacheResourcesAsync() {
+    
+
+
+    const images = [];
+
+    Object.keys(ImageSources).forEach(key => {
+      picObj = ImageSources[key]
+
+      if(Object.prototype.toString.call(picObj.uri) == "[object Object]") {
+        return Object.keys(picObj.uri).forEach(key => images.push(picObj.uri[key]))
+      }
+      return images.push(picObj.uri) 
+    });
+
+    const cacheImages = images.map((image) => Asset.loadAsync(image))
+
+    return Promise.all(cacheImages)
+
   }
 }
