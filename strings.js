d2782aa3c531c16d037dd8fcae7bce08b53a228c
Alessandro Petrillo <aless.petrillo@gmail.com>
2019-03-05 16:00:33 +0100

    Fallback language: english. Add more strings for the alert messages

--- strings.js
+++ strings.js
@@ -10,7 +10,8 @@ let de = require('./strings/de.json');
 let en = require('./strings/en.json');
 
 i18n.fallbacks = true;
+i18n.defaultLocale = "en";
 i18n.translations = {en, de};
 i18n.locale = Localization.locale; 
 
-export {i18n, Capitalize };
+export {i18n, Capitalize }; 
